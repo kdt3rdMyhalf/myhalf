7abe66c1be41f55cbca92534402315adfb6784ff
오성인 <dhtjddls12@naver.com>
2022-11-17 09:34:56 +0900

    sequelize, multer  모듈 등록 및 기본 세팅

---
+++ models/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const fs = require('fs');
+// const path = require('path');
+// const process = require('process');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+const Sequelize = require('sequelize');
+const config = require(__dirname + '/../config/config.json')['development'];
+const db = {};
+
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+)
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// 테이블 등록
+// db.뭐시기 = require('./모델파일')(sequelize, Sequelize);
+
+module.exports = db;
