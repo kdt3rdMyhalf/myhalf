8948d6091f84cf507f2c650e42dc36308d2473e1
JaeinChoii <hightreehuman@gmail.com>
2022-11-28 15:25:43 +0900

    comment feature modified

--- static/js/commu_post.js
+++ static/js/commu_post.js
@@ -1,3 +1,5 @@
+
+
 // 좋아요 눌렀을 때
 const postLikesBox = document.querySelector(".postLikesBox");
 
@@ -46,17 +48,18 @@ function commentPost(postId) {
       console.log("data>>>", data);
 
       const html = `      
-      <div class="commentInfoBox">
+      <div class="commentInfoBox id${data.commId}">
         <div class="commentInfoDiv1">
           <div class="commentWriter">${data.userName}</div>
           <div class="commentDate">${data.commDate}</div>
+          <div class="commentsContent">${data.commDoc}</div>
         </div>
-        <div class="commentInfoDiv2">
-          <button type="button" class="commentEditBtn">수정</button>
-          <button type="button" class="commentDeleteBtn">삭제</button>
-        </div>
+        <div class="commentInfoDiv2${data.commId}">
+          <button type="button" onclick="editComment('${data.commId}')" class="commentEditBtn">수정</button>
+          <button type="button" onclick="deleteComment(this, '${data.commId}')" class="commentDeleteBtn">삭제</button>
+          </div>
       </div>
-      <div class="commentsContent">${data.commDoc}</div>`;
+      `
 
       comments.insertAdjacentHTML("beforeend", html);
       clearInput();
@@ -108,3 +111,79 @@ function clearInput() {
 let postDoc = document.querySelector('.postDoc').innerText
 let postContent = document.querySelector('.postContent')
 postContent.innerHTML = postDoc;
+
+
+// 게시글 댓글 수정
+function editComment(commId) {
+  console.log('수정버튼 클릭!');
+  let commBtnBox = document.querySelector(`.commentInfoDiv2${commId}`);
+  let commText = document.querySelector(`.id${commId} .commentsContent`).textContent;
+  console.log(commText)
+  const html = `<br>
+  <input type="text" class="edit${commId}" value='${commText}'>
+  <button type="button" class="commentEditBtn" onclick="editCommentDo('${commId}')">등록</button>
+  <button type="button" class="commentDeleteBtn" onclick="editCommentCancel('${commId}')">취소</button>`
+  commBtnBox.innerHTML = html;
+};
+
+function editCommentCancel(commId) {
+  let commBtnBox = document.querySelector(`.commentInfoDiv2${commId}`);
+
+  const html = `
+  <button type="button" class="commentEditBtn" onclick="editComment('${commId}')">수정</button>
+  <button type="button" class="commentDeleteBtn" onclick="deleteComment(this, '${commId}')">삭제</button>`
+  
+  commBtnBox.innerHTML = html;
+}
+
+function editCommentDo(commId) {
+  let commValue = document.querySelector(`.edit${commId}`).value;
+  let now = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+  axios({
+    method: 'POST',
+    url: '/commu/comment/update',
+    data: {
+      commId: commId,
+      commValue: commValue,
+      commDate: now
+    }
+  }).then((result) => {
+    let commBtnBox = document.querySelector(`.commentInfoDiv2${commId}`);
+    let commText = document.querySelector(`.id${commId} .commentsContent`);
+    let commentDate = document.querySelector(`.id${commId} .commentDate`);
+    console.log(commValue);
+    commText.innerText = `${commValue}`;
+    commentDate.innerText = `${now}`;
+    commBtnBox.innerHTML = `
+    <button type="button" class="commentEditBtn" onclick="editComment('${commId}')">수정</button>
+    <button type="button" class="commentDeleteBtn" onclick="deleteComment(this, '${commId}')">삭제</button>`
+  })
+}
+
+
+
+// 게시글 댓글 삭제
+async function deleteComment(obj, commId) {
+  console.log('삭제버튼 클릭!');
+  console.log('obj', obj);
+  console.log('commId', commId);
+  if (!confirm('댓글을 삭제하시겠습니까?')) {
+    return;
+  }
+
+  axios({
+    method: 'DELETE',
+    url: `/commu/posts/:postId/deletecomment`,
+    data: {
+      commId: commId,
+    },
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .then((data) => {
+      alert(data);
+      obj.parentElement.parentElement.remove();
+    });
+};
